da4fb9e1d4eb88dd1555243385795a97ba88731e
murat-polat <muartsdepo@gmail.com>
2020-08-16 10:36:23 +0200

    first

---
+++ tutor-wagtail/README.rst
@@ -0,0 +1,22 @@
+wagtail plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/murat-polat/tutor-wagtail
+
+Usage
+-----
+
+::
+
+    tutor plugins enable wagtail
+    
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ tutor-wagtail/setup.py
@@ -0,0 +1,49 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, "README.rst"), "rt", encoding="utf8") as f:
+    readme = f.read()
+
+about = {}
+with io.open(
+    os.path.join(here, "tutorwagtail", "__about__.py"),
+    "rt",
+    encoding="utf-8",
+) as f:
+    exec(f.read(), about)
+
+setup(
+    name="tutor-wagtail",
+    version=about["__version__"],
+    url="https://github.com/murat-polat/tutor-wagtail",
+    project_urls={
+        "Code": "https://github.com/murat-polat/tutor-wagtail",
+        "Issue tracker": "https://github.com/murat-polat/tutor-wagtail/issues",
+    },
+    license="AGPLv3",
+    author="MP",
+    description="wagtail plugin for Tutor",
+    long_description=readme,
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.5",
+    install_requires=["tutor-openedx"],
+    entry_points={
+        "tutor.plugin.v0": [
+            "wagtail = tutorwagtail.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
